415c5e87aa71c03f66217e1215a9e06145af6da0
mahdi <mahdi.montazeri.dev@gmail.com>
2024-04-19 14:34:41 +0330

    create basic file

---
+++ src/core/http-service.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "https://react-mini-projects-api.classbon.com";
+
+export const httpService = axios.create({
+  baseURL: BASE_URL,
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+})
+
+
---
+++ src/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./features/identity/components/login";
+import Register, { registerAction } from "./features/identity/components/register";
+import IdentityLayout from "./layouts/indentity-layout";
+
+const router = createBrowserRouter([
+  {
+    element: <IdentityLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register />,action: registerAction ,errorElement:<Register/>},
+    ],
+  },
+]);
+export default router;
